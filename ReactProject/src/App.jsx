d391553a51113eec1595e378a8a2287f45975bdd
Abanoub Isaac <abanoubisaacai@gmail.com>
2025-04-08 17:10:08 +0200

    .

--- ReactProject/src/App.jsx
+++ ReactProject/src/App.jsx
@@ -3,11 +3,21 @@
 import Logo from "./components/Logo";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
+import Home from "./Pages/Home";
+import Shop from "./Pages/Shop";
+import Dashboard from "./Pages/Dashboard";
 
+import { BrowserRouter, Route, Routes } from "react-router";
 function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/shop" element={<Shop></Shop>}></Route>
+          <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ ReactProject/src/Pages/Home.jsx
@@ -0,0 +1,6 @@
+import Navbar from "../components/Navbar";
+function Home() {
+  return <Navbar></Navbar>;
+}
+
+export default Home;
