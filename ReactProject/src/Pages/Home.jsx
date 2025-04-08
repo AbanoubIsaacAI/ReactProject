6788ea392cbf80cc0c352d98defea3f90e437d8b
Abanoub Isaac <abanoubisaacai@gmail.com>
2025-04-08 19:55:19 +0200

    .

--- ReactProject/src/Pages/Home.jsx
+++ ReactProject/src/Pages/Home.jsx
@@ -1,6 +1,16 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 function Home() {
-  return <Navbar></Navbar>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <Slider></Slider>
+      <Products></Products>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default Home;
