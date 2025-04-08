dfe2c8e792f1d2961e02944c797a586ff51b0dd6
2bohany <youssef2000649@gmail.com>
2025-04-08 19:56:46 +0200

    .

--- ReactProject/src/Pages/Dashboard.jsx
+++ ReactProject/src/Pages/Dashboard.jsx
@@ -1,5 +1,15 @@
+
+import BarChart from "../Components/BarChart.jsx";
+import PieChart from "../Components/PieChart.jsx";
+import LineChart from "../components/LineChart.jsx";
+import DoughnutChart from "../components/DoughnutChart.jsx";
 function Dashboard() {
-  return <div></div>;
+  return <div>
+    <BarChart></BarChart>
+    <PieChart></PieChart>
+    <LineChart></LineChart>
+    <DoughnutChart></DoughnutChart>
+  </div>;
 }
 
 export default Dashboard;
