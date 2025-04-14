9707d5f7efb8e2652950b60155118284223502b6
HadeerMamdouh <mamhadeer@gmail.com>
2025-04-14 14:46:09 +0200

    added landing page

--- ReactProject/src/App.jsx
+++ ReactProject/src/App.jsx
@@ -11,6 +11,7 @@ import { useState } from "react";
 import Offers from "./Pages/Offers";
 import SignUp from "./Pages/SignUp";
 import Login from "./Pages/Login";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -21,6 +22,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route
+          path="/"
+          element={
+            <LandingPage
+              cart={cart}
+              setCart={setCart}
+              wishlist={wishlist}
+              setWishlist={setWishlist}
+            />
+          }
+        />
+
         <Route
           path="/"
           element={
--- ReactProject/src/components/Navbar.jsx
+++ ReactProject/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Search from "./Search";
 import { useContext, useState } from "react";
 import { UserContext } from "../Pages/User";
+import logoImg from "../assets/logo1.png";
 
 function Navbar({ cart, setCart, wishlist, setWishlist }) {
   const navigate = useNavigate();
@@ -81,39 +82,56 @@ function Navbar({ cart, setCart, wishlist, setWishlist }) {
   }
 
   return (
-    <div className="navbar shadow-sm sticky top-0 z-50 bg-[#1B6392]">
+    <div className="navbar shadow-sm sticky top-0 z-50 bg-[#001937] px-4 md:px-16">
       <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">
-          E-Shop
+        <Link to="/" className="btn btn-ghost p-0">
+          <img src={logoImg} alt="Logo" className="h-10 md:h-12" />
         </Link>
       </div>
 
-      <div className="flex items-center gap-4">
-        <Search />
+      <div className="flex items-center gap-4 md:gap-4">
+        <div>
+          <Search />
+        </div>
 
         {/* Wishlist Dropdown */}
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
             <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill={wishlist.length ? "red" : "none"}
-                viewBox="0 0 24 24"
-                stroke={wishlist.length ? "red" : "currentColor"}
-                strokeWidth={2}
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 6.01 4.01 4 6.5 4c1.74 0 3.41 1.01 4.13 2.44h1.74C14.09 5.01 15.76 4 17.5 4 19.99 4 22 6.01 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-                />
-              </svg>
-              {wishlist.length > 0 && (
-                <span className="badge badge-sm indicator-item">
-                  {wishlist.length}
-                </span>
+              {wishlist.length ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="red"
+                  viewBox="0 0 24 24"
+                  stroke="red"
+                  strokeWidth={2}
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 6.01 4.01 4 6.5 4c1.74 0 3.41 1.01 4.13 2.44h1.74C14.09 5.01 15.76 4 17.5 4 19.99 4 22 6.01 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  className="h-6 w-6 text-red-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 6.01 4.01 4 6.5 4c1.74 0 3.41 1.01 4.13 2.44h1.74C14.09 5.01 15.76 4 17.5 4 19.99 4 22 6.01 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                  />
+                </svg>
               )}
+              <span className="badge badge-sm indicator-item">
+                {wishlist.length}
+              </span>
             </div>
           </div>
 
@@ -139,7 +157,12 @@ function Navbar({ cart, setCart, wishlist, setWishlist }) {
                     />
                   </svg>
                   <span>Your favourite list is empty</span>
-                  <Link to="/shop" className="w-full">
+                  <img
+                    src="https://img.icons8.com/?size=100&id=117006&format=png&color=000000"
+                    alt="Empty favourites"
+                    className="w-50 relative left-[30%] translate-x-[-50%]"
+                  />
+                  <Link to={`/shop`} className="w-full">
                     <button
                       className="btn w-50 text-white"
                       style={{ backgroundColor: "#FA8232" }}
@@ -155,6 +178,7 @@ function Navbar({ cart, setCart, wishlist, setWishlist }) {
                       ? `${wishlist.length} Item`
                       : `${wishlist.length} Items`}
                   </span>
+
                   {wishlist.map((product) => (
                     <div
                       className="flex items-center gap-4 p-3 rounded-lg border border-gray-200 shadow-sm hover:shadow-md transition"
@@ -206,7 +230,7 @@ function Navbar({ cart, setCart, wishlist, setWishlist }) {
                 className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
+                stroke="#E0045D"
               >
                 <path
                   strokeLinecap="round"
@@ -215,111 +239,117 @@ function Navbar({ cart, setCart, wishlist, setWishlist }) {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              {cart.length > 0 && (
-                <span className="badge badge-sm indicator-item">
-                  {cart.length}
-                </span>
-              )}
+              <span className="badge badge-sm indicator-item">
+                {cart.length}
+              </span>
             </div>
           </div>
-
           <div
             tabIndex={0}
             className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-110 shadow"
           >
-            <div className="card-body h-96 overflow-y-auto text-center">
-              {cart.length === 0 ? (
-                <>
-                  <span className="font-bold text-2xl">Your cart is empty</span>
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/11329/11329060.png"
-                    alt="Empty cart"
-                    className="w-50 relative left-[50%] translate-x-[-50%]"
-                  />
-                  <Link to="/shop" className="w-full">
-                    <button
-                      className="btn w-full mt-2 text-white"
-                      style={{ backgroundColor: "#FA8232" }}
-                    >
-                      Start shopping
-                    </button>
-                  </Link>
-                </>
-              ) : (
-                <>
-                  <span className="text-lg font-bold text-center">
-                    {cart.length === 1
-                      ? `${cart.length} Item`
-                      : `${cart.length} Items`}
-                  </span>
-                  {cart.map((product) => (
-                    <div
-                      className="flex justify-between gap-3"
-                      key={product.id}
-                    >
-                      <img src={product.image} alt="" className="h-26" />
-                      <div className="flex flex-col justify-between basis-90 pb-1">
-                        <div>
-                          <h6>{product.title}</h6>
-                        </div>
-                        <div>
-                          <button
-                            disabled={product.isMaxCount}
-                            className="btn bg-blue-400 rounded-full p-0 h-8 w-8 mr-1 text-white"
-                            onClick={() => handleIncProductCount(product.id)}
-                          >
-                            +
-                          </button>
-                          <span>Count: {product.counter}</span>
-                          {product.counter === 1 ? (
-                            <button
-                              className="btn bg-blue-400 hover:bg-red-500 rounded-full p-0 h-8 w-8 ml-1 text-white transition-colors duration-200"
-                              onClick={() => handleRemoveFromCart(product.id)}
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="h-4 w-4"
-                              >
-                                <path d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                              </svg>
-                            </button>
-                          ) : (
-                            <button
-                              className="btn bg-blue-400 rounded-full p-0 h-8 w-8 ml-1 text-white"
-                              onClick={() => handleDecProductCount(product.id)}
-                            >
-                              -
-                            </button>
-                          )}
-                          {product.isMaxCount && (
-                            <p className="text-red-600">
-                              That's the maximum available quantity
-                            </p>
-                          )}
-                        </div>
-                        <p className="text-orange-400">
-                          Price: {product.finalPrice}$
-                        </p>
-                      </div>{" "}
-                      <div>
+            <div className="card-body h-96 overflow-y-auto">
+              <span className="text-lg font-bold text-center">
+                {cart.length === 0 && (
+                  <>
+                    <span>Your cart is empty</span>
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/11329/11329060.png"
+                      alt="Empty cart"
+                      className="w-50 relative left-[50%] translate-x-[-50%]"
+                    />
+                    <Link to={`/shop`} className="w-full">
+                      <button
+                        className="btn w-full mt-2 text-white"
+                        style={{ backgroundColor: "#FA8232" }}
+                      >
+                        Start shoping
+                      </button>
+                    </Link>
+                  </>
+                )}
+                {cart.length === 1 && `${cart.length} Item`}
+                {cart.length > 1 && `${cart.length} Items`}
+              </span>
+              {cart.map((product) => (
+                <div className="flex justify-between gap-3" key={product.id}>
+                  <img src={product.image} alt="" className="h-26" />
+                  <div className="flex flex-col justify-between basis-90 pb-1">
+                    <div>
+                      <h6>{product.title}</h6>
+                    </div>
+                    <div>
+                      <button
+                        disabled={product.isMaxCount}
+                        className="btn bg-blue-400 rounded-full p-0 h-8 w-8 mr-1 text-white"
+                        onClick={() => handleIncProductCount(product.id)}
+                      >
+                        +
+                      </button>
+                      <span>Count: {product.counter}</span>
+                      {product.counter === 1 ? (
                         <button
-                          className="cursor-pointer"
+                          className="btn bg-blue-400 rounded-full p-0 h-8 w-8 ml-1 text-white"
                           onClick={() => handleRemoveFromCart(product.id)}
                         >
-                          ❌
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6 text-white hover:text-red-500 cursor-pointer transition-colors"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                            />
+                          </svg>
                         </button>
-                      </div>
+                      ) : (
+                        <button
+                          className="btn bg-blue-400 rounded-full p-0 h-8 w-8 ml-1 text-white"
+                          onClick={() => handleDecProductCount(product.id)}
+                        >
+                          -
+                        </button>
+                      )}
+                      {product.isMaxCount ? (
+                        <p className="text-red-600">
+                          That's the maximum available quantity of the product
+                        </p>
+                      ) : (
+                        ""
+                      )}
                     </div>
-                  ))}
-                  <span className="text-info text-center">
-                    Total: {cart.reduce((a, p) => a + p.finalPrice, 0)}$
-                  </span>
+                    <div>
+                      {" "}
+                      <p className="text-orange-400">
+                        Price: {product.finalPrice}$
+                      </p>
+                    </div>
+                  </div>
+                  <div>
+                    <button
+                      className="cursor-pointer"
+                      onClick={() => handleRemoveFromCart(product.id)}
+                    >
+                      ❌
+                    </button>
+                  </div>
+                </div>
+              ))}
+              {cart.length != 0 ? (
+                <span className="text-info text-center">
+                  Total:
+                  {cart.reduce((a, p) => a + p.finalPrice, 0)}$
+                </span>
+              ) : (
+                ""
+              )}
+              <div className="card-actions">
+                {
                   <Link
                     to="/cart"
                     className="btn btn-block text-white"
@@ -327,8 +357,8 @@ function Navbar({ cart, setCart, wishlist, setWishlist }) {
                   >
                     View Cart
                   </Link>
-                </>
-              )}
+                }
+              </div>
             </div>
           </div>
         </div>
@@ -342,12 +372,11 @@ function Navbar({ cart, setCart, wishlist, setWishlist }) {
           >
             <div className="w-10 rounded-full">
               <img
-                alt="Profile"
+                alt="Tailwind CSS Navbar component"
                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
               />
             </div>
           </div>
-
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
